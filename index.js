7f57d4c1b3d35c8308833619addb7916e2747be1
emon-webdev <emon.webdev@gmail.com>
2022-10-26 10:18:11 +0600

    single course dynamic

--- index.js
+++ index.js
@@ -4,18 +4,16 @@ const cors = require("cors");
 app.use(cors());
 
 const Port = process.env.Port || 5000;
-
 const allCourse = require("./data/courses.json");
 
 app.get("/", (req, res) => {
   res.send("Show Pro Learner in UI....");
 });
-
 //all data send ui
 app.get("/courses", (req, res) => {
   res.send(allCourse);
 });
-    
+
 //single courses
 app.get("/courses/:id", (req, res) => {
   const id = req.params.id;
